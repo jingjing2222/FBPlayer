a70085512131b30b4b32a7a0c087be3e2e7e6be8
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2025-01-29 23:56:01 +0900

    Refactor: 구조 변경, 가독성 향상

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,16 +1,20 @@
-import { ErrorProvider } from "@/context/ErrorContext";
-import { InputForm } from "@/components/form/InputForm";
-import SearchButton from "@/components/form/SearchButton";
 import SelectPosition from "@/components/form/SelectPosition";
 import ViewList from "@/components/form/ViewList";
+import Nationality from "@/mock/Nationality";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export default function Form() {
     console.log("Form re-rendered");
 
-    const [result, setResult] = useState([]);
-    const { register, handleSubmit, setValue, getValues } = useForm({
+    const [result] = useState([]);
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        formState: { errors },
+    } = useForm({
+        mode: "onSubmit",
         defaultValues: {
             fullname: "",
             nationality: "",
@@ -19,37 +23,45 @@ export default function Form() {
     });
 
     function searchClick(data) {
-        console.log(data);
+        const newData = { ...data, nationality: Nationality[data.nationality] };
+        console.log(newData);
     }
 
     return (
-        <>
-            <form className="flex flex-col border-4 p-2 border-black bg-lime-700">
-                <ErrorProvider>
-                    <InputForm
-                        register={register}
-                        placeholder={"이름"}
-                        objectKey={"fullname"}
-                    />
-                    <InputForm
-                        register={register}
-                        placeholder={"국가"}
-                        objectKey={"nationality"}
-                    />
-
-                    <div className="flex-initial"></div>
-                    <SelectPosition setValue={setValue} />
-                    <SearchButton
-                        handleSubmit={handleSubmit}
-                        getValues={getValues}
-                        setValue={setValue}
-                        searchClick={searchClick}
-                    />
-                    <div className="flex flex-col justify-center items-center">
-                        <ViewList result={result} />
-                    </div>
-                </ErrorProvider>
-            </form>
-        </>
+        <form className="flex flex-col border-4 p-2 border-black bg-lime-700">
+            <input
+                type="text"
+                placeholder={"이름"}
+                className="flex-initial m-2 rounded-md px-1 border-black border-2"
+                {...register("fullname")}
+            />
+            <input
+                type="text"
+                placeholder={"국가"}
+                className="flex-initial m-2 rounded-md px-1 border-black border-2"
+                {...register("nationality", {
+                    validate: {
+                        incorrectnationality: (data) =>
+                            data.length > 0 &&
+                            !(data in Nationality) &&
+                            "국가명 없음",
+                    },
+                })}
+            />
+            <div className="text-center text-red-500 mb-2">
+                {errors.nationality?.message}
+            </div>
+            <div className="flex-initial"></div>
+            <SelectPosition setValue={setValue} />
+            <button
+                className="flex-initial w-auto bg-slate-400 border-2 border-black"
+                onClick={handleSubmit(searchClick)}
+            >
+                검색
+            </button>
+            <div className="flex flex-col justify-center items-center">
+                <ViewList result={result} />
+            </div>
+        </form>
     );
 }
